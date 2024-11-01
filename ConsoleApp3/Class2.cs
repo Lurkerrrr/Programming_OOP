39714909b832e97daca605c94d8214bc8ee0ae09
Viktor <95305510+Lurkerrrr@users.noreply.github.com>
2024-11-01 17:22:22 +0100

    class

---
+++ ConsoleApp3/Class2.cs
@@ -0,0 +1,33 @@
+﻿using System; 
+using SharpLab
+
+namespace SharpLab
+{
+    internal class Person
+    {
+        private string firstName, lastName; 
+        private int age; 
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName; 
+            this.lastName = lastName;   
+            this.age = age;
+        } 
+
+        public void View()
+        {
+            Console.WriteLine($"Imie: \t{firstName}\tNazwisko:\t{lastName}\tWiek:\t{age}"
+        }
+    }
+} 
+
+using SharpLab;
+
+Person[] people = new Person[]
+{
+    new Person("Jan", "Nowak",34),
+    new Person("Jana", "Nowak",36),
+    new Person("Ala", "Jankowska",56),
+    new Person("Maria", "Kowalska",18),
+};
---
+++ ConsoleApp3/Class3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SharpLab
+
+namespace SharpLab
+{
+    internal class Book
+    {
+        private string title;
+        Person author;
+        private DateTime datePublication;
+
+        public global::System.String Title { get => title; set => title = value; }
+        public Person Author { get => author; set => author = value; }
+        public DateTime DatePublication { get => datePublication; set => datePublication = value; }
+
+        // 00/00/0000 00:00:00 
+
+        public void View()
+        {
+            Console.WriteLine($"Tytul:\t{Title}" + $"\nAuthor:\t";
+            Author.View();
+            Console.WriteLine("\ndata wydania:\t{datePublication}");
+        }
+    }
+    public Book(string title, Person author, DateTime datePublication)
+    {
+        this.title = title;
+        this.author = author;
+        this.datePublication = datePublication;
+    }
+}
