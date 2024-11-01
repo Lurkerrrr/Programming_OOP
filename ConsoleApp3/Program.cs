45cc734c90e42392f65db91dd15319125efc0e21
Viktor <95305510+Lurkerrrr@users.noreply.github.com>
2024-11-01 17:21:09 +0100

    main

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,62 @@
+﻿public class Osoba
+{
+    private string imie;
+
+    public string Imie
+    {
+        get { return imie; }
+        set { imie = value; }
+
+    public required string Imie { get; set }
+
+    public string Imie { get; private set; }
+
+    Pojazd pojazd = new Pojazd();
+    pojazd.Uruchom(); 
+
+Samochod samochod = new Samochod();
+    samochod.Klakson();
+    samochod.Uruchom();
+
+class Pojazd
+    {
+        public void Uruchom()
+        {
+            Console.WriteLine($"Pojazd uruchomiony");
+        }
+    }
+
+    class Samochod : Pojazd
+    {
+        public void Klakson()
+        {
+            Console.WriteLine($"Samochod trabi");
+        }
+
+    }
+
+    class Osoba
+    {
+        protected string Imie { get; set; }
+
+        public Osoba(string imie)
+        {
+            Imie = imie;
+        }
+    }
+
+    public class Pracownik : Osoba
+    {
+        public string Stanowisko { get; set; }
+        public Pracownik(string imie, string stanowisko) : base(imie)
+        {
+            Stanowisko = stanowisko;
+
+        }
+
+
+    }
+}
+
+
+
