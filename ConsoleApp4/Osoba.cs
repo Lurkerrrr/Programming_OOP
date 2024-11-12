7abf2e164b55c79413f7bef799ccefbddd2668a9
Viktor <95305510+Lurkerrrr@users.noreply.github.com>
2024-11-12 18:13:04 +0100

    class

---
+++ ConsoleApp4/Osoba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Osoba : IOsoba
+{
+    public string Imię { get; set; }
+    public string Nazwisko { get; set; }
+
+    public Osoba(string imię, string nazwisko)
+    {
+        Imię = imię;
+        Nazwisko = nazwisko;
+    }
+
+    public string FullData()
+    {
+        return $"{Imię} {Nazwisko}";
+    }
+}
