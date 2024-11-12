f84ade60a6188ccbe20e5b0ce10d70f07631828a
Viktor <95305510+Lurkerrrr@users.noreply.github.com>
2024-11-12 18:13:46 +0100

    class

---
+++ ConsoleApp4/StudentWSIiZ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class StudentWSIiZ : Student
+{
+    public StudentWSIiZ(string imię, string nazwisko, string kierunek, int rok, int semestr)
+        : base(imię, nazwisko, "WSIiZ", kierunek, rok, semestr)
+    {
+    }
+
+    public new string WypiszPelnaNazweIUczelnie()
+    {
+        return $"{ZwrocPelnaNazwe()} – {Rok}{Semestr} {Kierunek} WSIiZ";
+    }
+}
